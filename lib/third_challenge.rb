1e6d8ce7bd9494742ec16d69f2b0c8bccdad41dc
Yury Spektor <yspektor78@gmail.com>
2019-11-27 15:05:35 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -4,10 +4,7 @@ def third_challenge
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
-      :hero_friends => [
-         {name: "Benvolio", age: "17", attitude: "worried"},
-         {name: "Mercutio", age: "18", attitude: "hot-headed"}
-      ]
+      :hero_friends => []
    },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
