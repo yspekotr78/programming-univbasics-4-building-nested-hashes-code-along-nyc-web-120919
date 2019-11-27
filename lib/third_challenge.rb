283c554294b34be790a4ee146f572df3cd208e2e
Yury Spektor <yspektor78@gmail.com>
2019-11-27 14:45:26 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -11,7 +11,8 @@ def third_challenge
         },
       :hero => {
         :name => "Romeo",
-        :age => "15"
+        :age => "15",
+        :status => "alive"
         },
       :hero_friends => []
    }, 
