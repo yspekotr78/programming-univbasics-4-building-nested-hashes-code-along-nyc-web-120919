e86a38c34e659bdc2e6fad54378888c092964d1e
Yury Spektor <yspektor78@gmail.com>
2019-11-27 14:57:16 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -24,17 +24,9 @@ def third_challenge
         :status => "alive"
         },
       :heroine_friends => [
-        {
-        :name => "Steven",
-        :age => "30",
-        :attitude => "confused"
-        },
-       {
-        :name => "Nurse",
-        :age => "44",
-        :attitude => "worried"
-       }
-     ]
+        {name: "Steven", age: "30", attitude: "confused"},
+        {name: "Nurse", age: "44", attitude: "worried"}
+      ]
    }
   }
 
