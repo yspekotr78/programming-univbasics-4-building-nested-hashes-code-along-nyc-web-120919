00fbdee0687b87b065ac13eb9f8dce222accedf5
Yury Spektor <yspektor78@gmail.com>
2019-11-27 14:53:24 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,30 +1,12 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {
-        :name => "Lord Montague",
-        :age => "53"
-        },
-      :matriarch => {
-        :name => "Lady Montague",
-        :age => "54"
-        },
-      :hero => {
-        :name => "Romeo",
-        :age => "15",
-        :status => "alive"
-        },
+      :patriarch => { :name => "Lord Montague", :age => "53" },
+      :matriarch => {:name => "Lady Montague",:age => "54"},
+      :hero => {:name => "Romeo",:age => "15",:status => "alive" },
       :hero_friends => [
-        {
-         :name => "Benvolio",
-         :age => "17",
-         :attitude => "worried"
-        },
-        {
-        :name => "Mercutio",
-        :age => "18",
-        :attitude => "hot-headed"
-        }
+        {:name => "Benvolio", :age => "17", :attitude => "worried"},
+        {:name => "Mercutio", :age => "18", :attitude => "hot-headed" }
       ]
    }, 
    :capulet => {
