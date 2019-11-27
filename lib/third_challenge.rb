0722436be173e7991d351c6130e0a87ca991d8d2
Yury Spektor <yspektor78@gmail.com>
2019-11-27 15:01:32 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,7 +1,10 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {name: "Lord Montague", age: "53"},
+      :patriarch => {
+      :name => "Lord Montague",
+      :age => "53"
+  },
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
