647c1416e3fa52ba8cab7875cad231d4e14cc25b
Yury Spektor <yspektor78@gmail.com>
2019-11-27 14:43:01 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -6,7 +6,8 @@ def third_challenge
         :age => "53"
         },
       :matriarch => {
-        #your key/value pairs here
+        :name => "Lady Montague",
+        :age => "54"
         },
       :hero => {
         #your key/value pairs here
